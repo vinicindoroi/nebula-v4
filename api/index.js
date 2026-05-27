13462d3348997bea3a19b15ad84a5ffd79ab2fd7
vinicindoroi <viniciobdf@gmail.com>
2026-05-27 16:33:17 -0300

    fix: exportar metodos HTTP nomeados para suporte nativo a Web Requests no Node.js da Vercel

--- api/index.js
+++ api/index.js
@@ -1,6 +1,29 @@
 import server from '../dist/server/server.js';
 
-export default async function handler(request) {
+export async function GET(request) {
   return server.fetch(request);
 }
 
+export async function POST(request) {
+  return server.fetch(request);
+}
+
+export async function PUT(request) {
+  return server.fetch(request);
+}
+
+export async function PATCH(request) {
+  return server.fetch(request);
+}
+
+export async function DELETE(request) {
+  return server.fetch(request);
+}
+
+export async function OPTIONS(request) {
+  return server.fetch(request);
+}
+
+export async function HEAD(request) {
+  return server.fetch(request);
+}
